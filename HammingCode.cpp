60eb951b02275f6d3c15cff69dd7ff0cbe00673d
9randeza <geydarb0412@gmail.com>
2024-03-30 12:49:02 +0400

    growUp well programm

--- HammingCode.cpp
+++ HammingCode.cpp
@@ -2,13 +2,13 @@
 #include <bitset>
 
 
-std::bitset<8> HammingCode(auto b){	
+short HammingCode(auto b){	
 	bool p1;
 	bool p2;
 	bool p3;
 	bool p;
 	
-	std::bitset<8> Hemming;
+	short Hemming;
 	
 	bool b1 = b & 0x1;
 	bool b2 = b & 0x2;
@@ -20,21 +20,32 @@ std::bitset<8> HammingCode(auto b){
 	p3 = (b2 + b3 + b4)%2 ;
 	p = (b1 + b2 + b3 +  b4 + p2 + p1 + p3 )%2; 
 
-	Hemming = (b1 << 2) + (b2 << 4) + (b4 << 6) +  (b3 << 5) + (p2 << 1) + p1  +( p3 << 3) +( p << 7);
+	Hemming = (b1 << 2) + (b2 << 4) + (b4 << 6) +  (b3 << 5) + (p2 << 1) + p1  + (p3 << 3) + (p << 7);
 	
 	return Hemming;
 }
 
+std::bitset<16> glue(auto b){
+	auto c = b;
+	auto a = b >> 4;
+	
+	short Hamming_1 = HammingCode(c);
+	short Hamming_2 = HammingCode(a) << 8;
+	std::bitset<16> Hamming = Hamming_1 + Hamming_2;
+	
+	
+	return Hamming;
+}
+
+
 int main() {
 
-	char  c = 'e' ;
-	int i = 9;
-	std::bitset<8>  h1 = HammingCode(c);
-	std::bitset<8>  h2 = HammingCode(i);
+	char  c = 't';
+	int i = 126;
 	std::bitset<8> t1(c);
 	std::bitset<8> t2(i);
-	std::cout << c << " : " << t1;
-	std::cout <<  " code Hemming 8/4: " << h1 << "\n";
+	std::cout << c << " : " << t1 << std::endl;
+	std::cout <<  " code Hemming 8/4: " << glue(c) << "\n";
 	std::cout << i << " : " << t2;
-	std::cout <<  " code Hemming 8/4: " << h2 << "\n";
+	std::cout <<  " code Hemming 8/4: " << glue(i) << "\n";
 }
