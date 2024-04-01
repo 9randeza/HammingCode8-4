6327d4d84c13392db596405374c76c61a7c16e1e
9randeza <geydarb0412@gmail.com>
2024-04-01 14:34:38 +0400

    change programm

--- HammingCode.cpp
+++ HammingCode.cpp
@@ -6,7 +6,7 @@ short HammingCode(auto b){
 	bool p1;
 	bool p2;
 	bool p3;
-	bool p;
+	bool p4;
 	
 	short Hemming;
 	
@@ -15,12 +15,12 @@ short HammingCode(auto b){
 	bool b3 = b & 0x4;
 	bool b4 = b & 0x8;
 
-	p1 = (b1 + b2 + b4)%2;
-	p2 = (b1 + b3 + b4)%2;
-	p3 = (b2 + b3 + b4)%2 ;
-	p = (b1 + b2 + b3 +  b4 + p2 + p1 + p3 )%2; 
+	p1 = (b1 + b3 + b4 + 1)%2;
+	p2 = (b1 + b2 + b4 + 1)%2;
+	p3 = (b1 + b2 + b3 + 1 )%2 ;
+	p4 = (b1 + b2 + b3 +  b4 + p2 + p1 + p3 + 1)%2; 
 
-	Hemming = (b1 << 3) + (b2 << 5) + (b4 << 7) +  (b3 << 6) + (p2 << 2) + (p1 << 1)  + (p3 << 4) + (p);
+	Hemming = (b1 << 1) + (b2 << 3) + (b4 << 7) +  (b3 << 5) + (p2 << 2) + (p1)  + (p3 << 4) + (p4 << 6);
 	
 	return Hemming;
 }
@@ -42,7 +42,7 @@ std::bitset<16> glue(auto b){
 int main() {
 
 	char  c = 'B';
-	int i = 9;
+	int i = 66;
 	std::bitset<8> t1(c);
 	std::bitset<8> t2(i);
 	std::cout << c << " : " << t1 << std::endl;
--- disCoding.cpp
+++ disCoding.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <bitset>
 
-int main(){
-	short c = 153;
+struct NotFixMistake{
+	std::string message;
+    NotFixMistake(const std::string& msg) : message(msg) {}
+};
+
+int DeCoder(int c){
 	
-	int g = c >> 8;
 	int t = c;
-	
 	std::bitset<8> c_1 = t;
-	std::bitset<8> c_2 = g;
-	
 	
 	bool b1 = t & 0x1;
 	bool b2 = t & 0x2;
@@ -20,32 +20,50 @@ int main(){
 	bool b7 = t & 0x40;
 	bool b8 = t & 0x80;
 	
-	bool s_0 = (b2 + b4 + b6 + b8)%2;
-	bool s_1 = (b3 + b4 + b7 + b8)%2;
-	bool s_2 = (b4 + b5 + b6 + b7)%2;
+	bool s_0 = (b1 + b2 + b6 + b8)%2;
+	bool s_1 = (b2 + b3 + b4 + b8)%2;
+	bool s_2 = (b2 + b4 + b5 + b6)%2;
 	bool p = (b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8)%2;
 	
+	int mask;
 	
-	if(s_0 == 0 && s_1 == 0 && s_2 == 0 && p == 0){
-		std::cout << "evrething good: " << c_1 << std::endl;
-	}
-	if((s_0 == 1 || s_1 == 1 || s_2 == 1) && p == 1){
-		std::cout << "some thing wrong\n";
-		int i = (s_0) + (s_1 << 1) + (s_2 << 2);
-		int mask = 0x80 >> i;
-		t ^= mask;
-		std::bitset<8> c_1 = t;
-		std::cout << s_0 << " " << s_1 << " " << s_2 << std::endl;
-		std::cout << "good version: " << c_1 << std::endl;
+	if((s_0 == 0 || s_1 == 0 || s_2 == 0) && p == 0){
+
+		if(s_0 == 0 && s_1 == 0 && s_2 == 0){
+			b2 ^= 1;
+		}
+		if(s_0 == 1 && s_1 == 0 && s_2 == 0){
+			b4 ^= 1;
+		}
+		if(s_0 = 0 && s_1 == 1 && s_2 == 0){
+			b6 ^= 1;
+		}
+		if(s_0 == 0 && s_1 == 0 && s_2 == 1){
+			b8 ^= 1;
+		}	
 
 	}
-	if((s_0 == 1 || s_1 == 1 || s_2 == 1) && p == 0){
-		std::cout << "something wrong, and I can`t fix it\n";
+	if((s_0 == 0 || s_1 == 0 || s_2 == 0) && p == 1){
+		throw NotFixMistake("something wrong, and I can`t fix it\n");
 	}
-	if(s_0 == 0 && s_1 == 0 && s_2 == 0 && p == 1){
-		std::cout << "p is wrong\n";
-		t ^= 0x1;
-		std::bitset<8> c_1 = t;
-		std::cout << "good version: " << c_1 << std::endl;
+	
+	int H = b2 + (b4 << 1) + (b6 << 2) + (b8 << 3);
+		
+	return H;
+}
+
+void glue(short c){
+	int g = c >> 8;
+	int t = c;
+	try{
+	int H = (DeCoder(g) << 4) + DeCoder(t);
+	std::cout << H << std::endl;
 	}
+	catch (const NotFixMistake& e) {
+        std::cout << e.message << std::endl;
+	}
+}
+int main(){
+	short c = 25673;
+	glue(c);
 }
